ab221dd31654bc46c33a2c447720428a87d1f5a9
Daniel Espindola da Silva <espindola.daniel@outlook.com.br>
2020-03-27 01:21:10 -0300

    responsividade adicionada. Sidebar agora é fixa em telas grandes

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ const date = (document.getElementById(
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+const body = document.getElementsByTagName('body')[0];
 // show nav
 navBtn.addEventListener("click", () => {
   navbar.classList.add("showNav");
+  body.classList.add("body-overflow");
 });
 // close nav
 navClose.addEventListener("click", () => {
   navbar.classList.remove("showNav");
+  body.classList.remove("body-overflow");
 });
