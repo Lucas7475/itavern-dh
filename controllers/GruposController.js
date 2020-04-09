09619672bd4a9e5065374da63018a7a2038bdf58
Daniel Espindola da Silva <espindola.daniel@outlook.com.br>
2020-04-09 11:07:32 -0300

    correção nas rotas e nos paths / get do form da procura de grupos

--- controllers/GruposController.js
+++ controllers/GruposController.js
@@ -1,7 +1,22 @@
-const grupos = require("../database/grupos.json");
+const gruposDB = require("../database/grupos.json");
 
 module.exports = {
-  index: (req, res) => {
+  search: (req, res) => {
+    let grupos = gruposDB
+
+    let {
+      searchText,
+      groupSize,
+      distance
+    } = req.query;
+
+    if (searchText) {
+      let res = grupos.filter(grupo => {
+        return (grupo.nome.includes(searchText) || grupo.jogo.includes(searchText))
+      })
+      grupos = res
+    }
+
     res.render("grupos", {
       grupos
     });
