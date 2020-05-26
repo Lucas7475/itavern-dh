f5c0fe0c21b53d275a01cd8c77f4b16718c30484
Lucas Pereira <lucas.p.silva90@hotmail.com>
2020-05-26 16:37:10 -0300

    criação dos midlewares de upload de imagem, ajuste do criar sala

--- routes/GruposRouter.js
+++ routes/GruposRouter.js
@@ -2,15 +2,12 @@ var express = require("express");
 var router = express.Router();
 
 const GruposController = require("../controllers/GruposController");
+const uploadCover = require('../middlewares/uploadCover');
 
 /* GET home page. */
 router.get("/", GruposController.index);
 router.get("/search/", GruposController.search);
-// router.get('/busca', GruposController.search);
-// router.get('/grupos/create', PizzasController.create);
-// router.post('/pizzas', PizzasController.store);
-// router.get('/grupos/:id', PizzasController.show);
+router.post('/criargrupo', uploadCover.single('imgGrupo'), GruposController.store);
 
-// router.post('/pedidos/add', PedidosController.add);
 
 module.exports = router;
