39bb910115b9dba0c38427a33c749e3986e365c9
Lucas Pereira <lucas.p.silva90@hotmail.com>
2020-06-04 13:46:21 -0300

    Criação de grupos

--- controllers/GruposController.js
+++ controllers/GruposController.js
@@ -10,12 +10,31 @@ module.exports = {
 
 
   store: async (req,res) => {
-
-    let imgGrupo = `public/images/covers/${req.file.filename}`;
-    let {nome, numJogador,inicioReuniao, horario, duracao, cep, descricao, diaReuniao} = req.body
-    console.log(nome,numJogador,inicioReuniao,horario,duracao,cep, descricao, diaReuniao);
-
-
+    let id_jogo = 1
+    let img = `public/images/covers/${req.file.filename}`;
+    let {nome,
+         numJogadores,
+         diasReuniao,
+         horario,
+         tempoJogo,
+         inicioReuniao,
+         cep,
+         numero,
+         descricao} = req.body
+    
+    diasReuniao = diasReuniao.toString();
+        
+    await Grupo.create({id_jogo,
+                        nome,
+                        numJogadores,
+                        diasReuniao,
+                        horario,
+                        tempoJogo,
+                        inicioReuniao,
+                        img,
+                        descricao,
+                        cep,
+                        numero});
   },
 
   search: (req, res) => {
