ee1a00dff7ed9b3b6462de291b092dc4fcb2382e
Lucas7475 <lucmate01@gmail.com>
2020-06-30 11:21:00 -0300

    Correção da info

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -17,35 +17,40 @@ const montaInfo = (dados)=>{
     popInfo.querySelector('#usuarios').innerText = '';
     popInfo.querySelector('#nomeGrupo').innerHTML = dados.nome;
     popInfo.querySelector('#dataInicio').innerHTML = dados.inicioReuniao;
-    dados.usuariosDoGrupo.forEach(usuario =>{
-        let articleUsuario = document.createElement('article');
-        articleUsuario.setAttribute('class','usuario');
-
-        let divImg = document.createElement('div');
-        divImg.setAttribute('class','imgUsuario');
-
-        let img = document.createElement('img');
-        img.setAttribute('id','avatar');
-        img.setAttribute('src', "https://vignette.wikia.nocookie.net/dungeonrampage/images/5/5e/Ghost_Samurai.png/revision/latest?cb=20140906175132" );
-
-        let label = document.createElement('label');
-        label.setAttribute('id','nickname');
-        label.innerText = usuario.nickname;
-
-        divImg.appendChild(img);
-        articleUsuario.appendChild(divImg);
-        articleUsuario.appendChild(label);
-
-        popInfo.querySelector('#usuarios').appendChild(articleUsuario);
-
-        if(usuario.id == dados.id_admin){
-            popInfo.querySelector('#nomeAdm').innerHTML = usuario.nickname;
-            popInfo.querySelector('.adm').classList.remove('invi');
-        }
-        else{
-            popInfo.querySelector('.adm').classList.add('invi');
-        }
-    })
+    if(dados.usuariosDoGrupo == []){
+        popInfo.querySelector('.adm').classList.add('invi');
+    }
+    else{
+        dados.usuariosDoGrupo.forEach(usuario =>{
+            let articleUsuario = document.createElement('article');
+            articleUsuario.setAttribute('class','usuario');
+    
+            let divImg = document.createElement('div');
+            divImg.setAttribute('class','imgUsuario');
+    
+            let img = document.createElement('img');
+            img.setAttribute('id','avatar');
+            img.setAttribute('src', "https://vignette.wikia.nocookie.net/dungeonrampage/images/5/5e/Ghost_Samurai.png/revision/latest?cb=20140906175132" );
+    
+            let label = document.createElement('label');
+            label.setAttribute('id','nickname');
+            label.innerText = usuario.nickname;
+    
+            divImg.appendChild(img);
+            articleUsuario.appendChild(divImg);
+            articleUsuario.appendChild(label);
+    
+            popInfo.querySelector('#usuarios').appendChild(articleUsuario);
+    
+            if(usuario.id == dados.id_admin){
+                popInfo.querySelector('#nomeAdm').innerHTML = usuario.nickname;
+                popInfo.querySelector('.adm').classList.remove('invi');
+            }
+            else{
+                popInfo.querySelector('.adm').classList.add('invi');
+            }
+        })
+    }
     pesquisaCepInfo(dados.cep);
     
 }
@@ -57,8 +62,13 @@ const montaInfo = (dados)=>{
 document.querySelectorAll('.info').forEach(botao => {
     botao.addEventListener('click', async (evento)=>{
         evento.preventDefault();
-            idGrupo = evento.composedPath()[2].id;
-            let dados = await requsitaDados(idGrupo);
-            montaInfo(dados);
+        idGrupo = evento.composedPath()[2].id;
+        let dados = await requsitaDados(idGrupo);
+        montaInfo(dados);
     })
 });
+
+document.getElementById('fechaInfo').addEventListener('click', evento=>{
+    evento.preventDefault();
+    document.querySelector('.adm').classList.add('invi');
+})
